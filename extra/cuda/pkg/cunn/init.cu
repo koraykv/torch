2848c119f8322973b6e8fffcaf3be241e62e4500
Leon Bottou <leon@bottou.org>
2013-04-11 15:31:09 -0400

    win: luaopen_xxx functions need LUA_EXTERNC

--- extra/cuda/pkg/cunn/init.cu
+++ extra/cuda/pkg/cunn/init.cu
@@ -24,7 +24,9 @@
 #include "Threshold.cu"
 #include "Sigmoid.cu"
 
-DLL_EXPORT int luaopen_libcunn(lua_State *L)
+LUA_EXTERNC DLL_EXPORT int luaopen_libcunn(lua_State *L);
+
+int luaopen_libcunn(lua_State *L)
 {
   lua_newtable(L);
 
